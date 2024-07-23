56558ec7be07500a69d05c20814e25af118b7a2f
HussamMohammed7 <146628050+HussamMohammed7@users.noreply.github.com>
2024-07-23 14:39:49 +0300

    Add files via upload

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,57 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+
+@RestController
+public class Controller {
+
+
+//    GET /name : returns “My name is .......”
+//            - GET /age : returns “My age is ........”
+//            - GET /check/status : returns “Everything OK”
+//            - GET /health : returns “Server health is up and running”
+//            - Get /names :
+//            return array of names
+
+
+
+    @GetMapping("/name")
+    public String name() {
+        return "My age is Hussam";
+    }
+
+    @GetMapping("/check/status")
+    public String everythingOK() {
+        return "Everything OK";
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "Server health is up and running";
+    }
+    @GetMapping("/names")
+    public ArrayList<String> names() {
+        ArrayList<String> names = new ArrayList<>();
+
+        names.add("hussam");
+        names.add("mohammed");
+        names.add("abdulaziz");
+        return names;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
